c5ac4d7fb3dd3a00661c723dce9e7eed018bcb39
Matt110110 <matruprasadchand@gmail.com>
2018-03-02 10:42:01 +0530

    Drop down menu is working.

--- app/src/main/java/io/github/teamseven/myvirtualplanner/MainActivity.java
+++ app/src/main/java/io/github/teamseven/myvirtualplanner/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,5 +31,21 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+//        Todo : Add some required functionality here
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.dropDown_settings :
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.dropDown_login :
+                Toast.makeText(this, "Log in / Sign up", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.dropDown_aboutUs:
+                Toast.makeText(this, "About us", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
