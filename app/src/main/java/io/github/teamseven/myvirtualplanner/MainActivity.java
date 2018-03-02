0a6a5179bca33dfdbe5ae5a5e04965adb9366fe9
Matt110110 <matruprasadchand@gmail.com>
2018-03-02 09:13:42 +0530

    Added toolbar

--- app/src/main/java/io/github/teamseven/myvirtualplanner/MainActivity.java
+++ app/src/main/java/io/github/teamseven/myvirtualplanner/MainActivity.java
@@ -2,12 +2,17 @@ package io.github.teamseven.myvirtualplanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.topToolbar);
+        setSupportActionBar(mToolbar);
+        mToolbar.setTitle(R.string.app_name);
     }
 }
